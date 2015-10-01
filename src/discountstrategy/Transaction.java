dbe5354edee85c750ad969c8bdc440e5c8057020
zsummers <zsummers@L11702042765.wctc.msft>
2015-10-01 16:24:28 -0500

    added addLineItem method

--- src/discountstrategy/Transaction.java
+++ src/discountstrategy/Transaction.java
@@ -10,5 +10,12 @@ package discountstrategy;
  * @author zsummers
  */
 public class Transaction {
+    private LineItem[] lineItems = new LineItem[0];
     
+    private void addLineItem(LineItem newItem){
+        LineItem[] temp = new LineItem[lineItems.length+1];
+        System.arraycopy(lineItems, 0, temp, 0, lineItems.length);
+        temp[lineItems.length] = newItem;
+        lineItems = temp;
+    }
 }
