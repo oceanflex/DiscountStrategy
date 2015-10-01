030075221959acdda56cf743c4ce5ce97d2b9410
zsummers <zsummers@L11702042765.wctc.msft>
2015-10-01 16:13:25 -0500

    Added writeItem and setter for qty

--- src/discountstrategy/LineItem.java
+++ src/discountstrategy/LineItem.java
@@ -12,16 +12,30 @@ package discountstrategy;
 public class LineItem {
     private Product product;
     private double qty;
-    private DataAccessStrategy db;
+    private final DataAccessStrategy db;
 
     public LineItem(String prodId, double qty, DataAccessStrategy db) {
         findProduct(prodId);
         this.qty = qty;
         this.db = db;
     }
+
+    public void setQty(double qty) {
+        this.qty = qty;
+    }
     
     private void findProduct (String prodId){
         this.product = db.findProduct(prodId);
+    }
+    
+    public String[] writeItem(){
+        String id = product.getProdId();
+        String name = product.getName();
+        String Quantity = "$"+ this.qty;
+        String cost = "$"+(product.getUnitPrice() * this.qty);
+        String discount ="$"+ product.getDiscountAmt(this.qty);
+        String[] back = {id, name, cost, Quantity, discount};
+        return back;
     }
     
 }
