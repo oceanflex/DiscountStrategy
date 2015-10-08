8de98c64cc8781569d4566ff050eebbbf1b9eb63
zsummers <zsummers@L11702042765.wctc.msft>
2015-10-08 16:47:54 -0500

    changed test

--- src/discountstrategy/FakeDatabase.java
+++ src/discountstrategy/FakeDatabase.java
@@ -25,7 +25,8 @@ public class FakeDatabase implements DataAccessStrategy {
     private Product[] products = {
         new Product("A101", "MLB Brewer's Hat ", 19.95, new PercentOffDiscount(0.15)),
         new Product("B205", "Men's Dress Shirt", 35.50, new QtyDiscount(.10,5)),
-        new Product("C222", "Women's Socks    ", 9.50, new NoDiscount())
+        new Product("C222", "Women's Socks    ", 9.50, new NoDiscount()),
+        new Product("B225", "Men's Socks      ", 8.50, new NoDiscount())
     };
     
     /**
--- src/discountstrategy/Startup.java
+++ src/discountstrategy/Startup.java
@@ -13,13 +13,20 @@ public class Startup {
     
     public static void main(String[] args) {
         String custId = "100";
-        String[] prodIds = {"A101","C222"};
+        String[] prodIds = {"A101","C222","B225","B205"};
         DataAccessStrategy db = new FakeDatabase();
         OutputStrategy receipt = new Receipt(db);
         Register register = new Register(db);
         register.newTransaction(custId, receipt);
         register.addItem(prodIds[0], 2);
         register.addItem(prodIds[1], 1);
+        register.endTransaction();
+        receipt = new Receipt(db);
+        custId = "200";
+        register.newTransaction(custId, receipt);
+        register.addItem(prodIds[2], 1);
+        register.addItem(prodIds[3], 3);
+        register.addItem(prodIds[0], 2);
         register.endTransaction();
         //String 
         
